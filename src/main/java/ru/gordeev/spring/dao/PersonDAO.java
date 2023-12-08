5d4cd1942753dfbb0d818902b43e1f0b636260bb
Vadim Gordeev <skillervrn@gmail.com>
2023-12-08 04:26:15 +0300

    grud dao with postresql

--- src/main/java/ru/gordeev/spring/dao/PersonDAO.java
+++ src/main/java/ru/gordeev/spring/dao/PersonDAO.java
@@ -3,41 +3,81 @@ package ru.gordeev.spring.dao;
 import org.springframework.stereotype.Component;
 import ru.gordeev.spring.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private List<Person> people;
     private static int PEOPLE_COUNT;
 
-    {
-        people = new ArrayList<>();
+    private static final String URL = "jdbc:postgresql:///first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "qwerty1234";
 
-        people.add(new Person(++PEOPLE_COUNT,"Tom", 32,"tom@tom.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Scotty", 18, "scotty@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Bob",23,"bobik@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Lola",27, "lolitochka@mail.ru"));
-    }
+    private static Connection connection;
 
-    public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
-    }
-    public void update(int id,Person updatePerson){
-        Person personToBeUpdated = show (id);
-        personToBeUpdated.setName(updatePerson.getName());
-        //personToBeUpdated.setSurname(updatePerson.getSurname());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
-    }
-    public void delete(int id){
-        people.removeIf(p -> p.getId() == id);
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Person> index (){
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return people;
     }
+
+    public void save(Person person){
+    //    person.setId(++PEOPLE_COUNT);
+    //    people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES("+ 1 +",'" + person.getName() + "'," + person.getAge() +
+            ",'" + person.getEmail() +"')";
+
+            statement.executeUpdate(SQL);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void update(int id,Person updatePerson){
+/*        Person personToBeUpdated = show (id);
+        personToBeUpdated.setName(updatePerson.getName());
+        //personToBeUpdated.setSurname(updatePerson.getSurname());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());*/
+    }
+    public void delete(int id){
+        //people.removeIf(p -> p.getId() == id);
+    }
+
+
 
     public Person show (int id) {
 
@@ -45,7 +85,7 @@ public class PersonDAO {
 //             ) {
 //            if (list.getId() == id) return list;
 //        }
-//        return null;
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
+        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 }
